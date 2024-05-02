8ff1390fe6a46fb8597dc77f08fe2e0e0b09fe3d
43alsosc <43alsosc@stud.akademiet.no>
2024-05-02 09:49:12 +0200

    Changed to fetch All todos, Done todos and not done todos

--- api/fetch/getDataTableData.tsx
+++ api/fetch/getDataTableData.tsx
@@ -1,10 +1,37 @@
 import { ToDo } from "@/components/data-table/columns";
 import { createClient } from "@/utils/supabase/server";
 
-export default async function getData(): Promise<ToDo[]> {
+export async function getData(): Promise<ToDo[]> {
   const supabase = createClient();
   supabase.auth.getUser();
 
+  const { data, error } = await supabase.from("todos").select("*");
+  if (error) {
+    console.error("Error fetching todos");
+    throw error;
+  } else if (!data) {
+    console.error("No data found");
+  }
+  return data;
+}
+
+export async function getDoneData(): Promise<ToDo[]> {
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("todos")
+    .select("*")
+    .match({ is_complete: true });
+  if (error) {
+    console.error("Error fetching todos");
+    throw error;
+  } else if (!data) {
+    console.error("No data found");
+  }
+  return data;
+}
+
+export async function getNotDoneData(): Promise<ToDo[]> {
+  const supabase = createClient();
   const { data, error } = await supabase
     .from("todos")
     .select("*")
